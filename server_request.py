2fc891affdbfca8204bad528ea22de62501bd07e
Hyde <>
2021-07-07 23:46:59 +0800

    add some error handle

--- server_request.py
+++ server_request.py
@@ -11,8 +11,11 @@ from nose.tools import assert_true
 class MyTestCase(unittest.TestCase):
     def test_getskutree(self):
         page = requests.get('https://apptest.perfectcorp.com/service/V2/getSkuTreeByType?country=US&lang=en-US&makeupVer=38.0&platform=iOS&product=YouCam%20Makeup&skuFormatVer=4.0&type=blush&version=1.0&versiontype=for%20iOS')
-        print(page.status_code)
         assert_true(page.ok)
+        if page.status_code==200:
+            print('getskutreeAPI is ok')
+        else:
+            print('getskutreeAPI has error')
 
 if __name__ == '__main__':
     unittest.main()
